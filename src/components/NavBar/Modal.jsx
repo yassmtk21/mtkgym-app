0d27fec19ed92102b9a09c3e2f0388faef6d51aa
yassmtk21 <yassinmoutik20@gmail.com>
2023-12-24 00:08:46 +0100

    Add form data validation to madal

--- src/components/NavBar/Modal.jsx
+++ src/components/NavBar/Modal.jsx
@@ -1,16 +1,73 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import background from "../../assets/background.jpg";
 import { ActionContext } from "../context/ActionContext";
 import { setIsModal } from "../actions/Actions";
 const Modal = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    gender: "",
+    email: "",
+    phone: "",
+    age: "",
+    cin: "",
+    errors: {},
+    loading: false,
+  });
   const { state, dispatch } = useContext(ActionContext);
   const openModal = state.isModal;
   if (!openModal) return null;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const validationRules = {
+    first_name: "first name required",
+    last_name: "last name required",
+    gender: "gender required",
+    email: "email required",
+    phone: "phone required",
+    age: "age required",
+    cin: "cin required",
+  };
 
+  const validateForm = () => {
+    const errors = {};
+
+    // Iterate over validation rules
+    Object.entries(validationRules).forEach(([fieldName, rule]) => {
+      if (!formData[fieldName]) {
+        errors[fieldName] = rule;
+      }
+    });
+
+    setFormData((prevState) => ({ ...prevState, errors }));
+    return Object.keys(errors).length === 0;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      setFormData({ ...formData, loading: true });
+      setTimeout(() => {
+        console.log(formData);
+        setFormData({ ...formData, loading: false });
+      }, 2000);
+    }
+  };
+
+  const handleExit = () => {
+    dispatch(setIsModal(false));
+  };
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 bottom-0 bg-[#000000] opacity-50 z-50 " />
-      <div className="flex flex-col lg:flex-row w-10/12 text-black lg:w-8/12 mx-auto shadow-lg overflow-hidden fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 rounded-lg transition-all duration-300">
+      <div className="fixed top-0 left-0 right-0 bottom-0 bg-[#000000] opacity-50 z-50 "></div>
+      {formData.loading && (
+        <p className="fixed top-4 left-[50%] translate-x-[-50%] bg-green-600 text-white font-bold z-[99999999] px-4 py-2">
+          register succesfuly{" "}
+        </p>
+      )}
+      <div className="flex flex-col lg:flex-row w-[90%] sm:w-[70%] text-black mx-auto shadow-lg overflow-hidden fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 rounded-lg">
         <div
           className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
           style={{ backgroundImage: `url(${background})` }}
@@ -18,25 +75,42 @@ const Modal = () => {
         <div className="w-full lg:w-1/2 py-16 px-12">
           <h2 className="text-3xl mb-4 text-black font-bold">Register</h2>
           <form>
-            <div className="grid grid-cols-2 gap-5">
-              <input
-                type="text"
-                placeholder="first name"
-                className="border border-gray-400 py-1 px-2"
-              />
-              <input
-                type="text"
-                placeholder="last name"
-                className="border border-gray-400 py-1 px-2"
-              />
-            </div>
-            <div className="grid grid-cols-2 mt-5">
-              <div className="w-1/2 ">
-                <h4 className="inline-block text-lg ">Gender : </h4>
+            <div className="grid md:grid-cols-2 gap-5">
+              <div>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  name="first_name"
+                  value={formData.first_name}
+                  placeholder="first name"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+                {formData.errors.first_name && (
+                  <p className="text-red-500">{formData.errors.first_name}</p>
+                )}
               </div>
-              <div className="flex">
+              <div>
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  name="last_name"
+                  value={formData.last_name}
+                  placeholder="last name"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+                {formData.errors.last_name && (
+                  <p className="text-red-500">{formData.errors.last_name}</p>
+                )}
+              </div>
+            </div>
+            <div className="grid md:grid-cols-2 mt-5">
+              <div className="w-1/2 ">
+                <h4 className="inline-block text-medium ">Gender : </h4>
+              </div>
+              <div className="">
                 <label className="mx-2">Male</label>
                 <input
+                  onChange={handleChange}
                   type="radio"
                   name="gender"
                   value={"male"}
@@ -44,54 +118,85 @@ const Modal = () => {
                 />
                 <label className="mx-2">female</label>
                 <input
+                  onChange={handleChange}
                   type="radio"
                   name="gender"
                   value={"female"}
                   className="cursor-pointer"
                 />
+
+                {formData.errors.gender && (
+                  <p className="text-red-500 block">{formData.errors.gender}</p>
+                )}
               </div>
             </div>
             <div className="mt-5">
               <input
+                onChange={handleChange}
                 type="email"
+                name="email"
+                value={formData.email}
                 placeholder="email"
                 className="border border-gray-400 py-1 px-2 w-full"
               />
+              {formData.errors.email && (
+                <p className="text-red-500">{formData.errors.email}</p>
+              )}
             </div>
             <div className="mt-5">
               <input
+                onChange={handleChange}
                 type="text"
+                name="phone"
+                value={formData.phone}
                 placeholder="phone"
                 className="border border-gray-400 py-1 px-2 w-full"
               />
+              {formData.errors.phone && (
+                <p className="text-red-500">{formData.errors.phone}</p>
+              )}
             </div>
-            <div className="grid grid-cols-2 gap-1 mt-5">
-              <div className="flex">
+            <div className=" mt-5 w-full">
+              <div className="w-full flex">
                 <label className="mr-2 text-lg">Age:</label>
                 <input
+                  onChange={handleChange}
                   type="date"
-                  className="border border-gray-400 py-1 px-2 "
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  placeholder="cin"
+                  name="age"
+                  value={formData.age}
                   className="border border-gray-400 py-1 px-2 w-full"
                 />
               </div>
+              {formData.errors.age && (
+                <p className="text-red-500">{formData.errors.age}</p>
+              )}
+              <div className="sm:full mt-5">
+                <input
+                  onChange={handleChange}
+                  type="text"
+                  name="cin"
+                  value={formData.cin}
+                  placeholder="cin"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+                <br />
+                {formData.errors.cin && (
+                  <p className="text-red-500">{formData.errors.cin}</p>
+                )}
+              </div>
             </div>
             <div className="mt-10 grid grid-cols-2 gap-5">
+              <input
+                onClick={handleSubmit}
+                type="submit"
+                value={"register"}
+                className="w-fulltext-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:px-5 px-2 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+              />
+
               <button
+                onClick={handleExit}
                 type="button"
-                className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-              >
-                Register
-              </button>
-              <button
-                onClick={() => dispatch(setIsModal(false))}
-                type="button"
-                className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                className="w-full text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm sm:px-5 px-2 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
               >
                 cancel
               </button>
